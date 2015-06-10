7219938349234f71224e3053d231e4e6123e7030
Chris Kahn <ckahn@users.noreply.github.com>
2015-06-09 18:08:29 -0700

    Remove more js dirs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,8 +17,3 @@
 //= require underscore
 //= require backbone
 //= require audiofile_app
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
